9f2312f2c3263d491ba5ed1cceaec13002c0b8a9
JesseDance <dance.jbd@gmail.com>
2024-10-16 11:40:01 -0500

    cleaned

--- Examples/Queue_Code_Examples/Queue_LinkedList.java
+++ Examples/Queue_Code_Examples/Queue_LinkedList.java
@@ -9,24 +9,23 @@ class Queue_LinkedList {
     public static void main(String args[])
     {
         // Creating empty LinkedList
-        Queue<Integer> ll
-            = new LinkedList<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
 
         // Adding items to the ll
         // using add()
-        ll.add(10);
-        ll.add(20);
-        ll.add(15);
+        queue.add(10);
+        queue.add(20);
+        queue.add(15);
 
         // Printing the top element of
         // the LinkedList
-        System.out.println(ll.peek());
+        System.out.println(queue.peek());
 
         // Printing the top element and removing it
         // from the LinkedList container
-        System.out.println(ll.poll());
+        System.out.println(queue.poll());
 
         // Printing the top element again
-        System.out.println(ll.peek());
+        System.out.println(queue.peek());
     }
 }
